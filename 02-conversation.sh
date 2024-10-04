cf86ea877fbad170a3d21cdf35513d78f1f893ff
spandana <gundaspandana68@gmail.com>
2024-10-04 19:13:55 -0400

    added conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi suresh,How are you?"
+echo "Suresh:: Hello Ramesh. I am fine . How are you doing?"
+echo "Ramesh:: I am doing good suresh, whats going on?"
+echo "Suresh:: I started learning Shell Script Ramesh"
