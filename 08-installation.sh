62bcefae3bab9284e883ce5fa6fc5b9c0eeee990
spandana <gundaspandana68@gmail.com>
2024-10-04 22:10:27 -0400

    added conversation

--- 08-installation.sh
+++ 08-installation.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 USERID=$(id -u)
-echo "user ID is: $USERID"
+#echo "user ID is: $USERID"
+
+if[ $USERID -ne o ]
+then
+    
+    echo "please run this script with root priveleges"
+
+fi
+ 
+dnf install git -y
